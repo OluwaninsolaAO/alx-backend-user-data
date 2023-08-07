24f3739b86662427440bd8e27b1928ea38bed0f7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-07 17:32:13 +0100

    Task 04

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,13 @@ class Auth:
     def require_auth(self, path: str,
                      excluded_paths: List[str]) -> bool:
         """require_auth public method"""
-        return False
+        if any([path is None, excluded_paths is None]):
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        if any([path in excluded_paths, path[:-1] in excluded_paths]):
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization_header public method"""
