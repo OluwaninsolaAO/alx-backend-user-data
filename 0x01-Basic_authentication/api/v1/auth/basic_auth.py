fae1e8819e4b0feb707d64b6cfed298ae616a237
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-08 08:14:47 +0100

    Task 08

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """6. Basic auth class definition"""
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode, binascii
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,17 @@ class BasicAuth(Auth):
         if authorization_header.split()[0] != 'Basic':
             return None
         return ''.join(authorization_header.split()[1:])
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode_base64_authorization_header public methon"""
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+        try:
+            decoded_str = b64decode(
+                base64_authorization_header.encode('utf-8'))
+            return decoded_str.decode('utf-8')
+        except binascii.Error:
+            return None
