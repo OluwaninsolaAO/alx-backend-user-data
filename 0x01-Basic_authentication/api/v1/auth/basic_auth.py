b8a8b891b597600fda9a724b8dd369771f09f0d6
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-08 09:03:50 +0100

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 
 from api.v1.auth.auth import Auth
 from base64 import b64decode, binascii
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -44,3 +46,18 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':')[:2])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """user_object_from_credentials public method"""
+        if any([user_email is None, user_pwd is None,
+                isinstance(user_email, str) is False,
+                isinstance(user_pwd, str) is False]):
+            return None
+        matches = User.search(attributes={'email': user_email})
+        if len(matches) == 0:
+            return None
+        for user in matches:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
