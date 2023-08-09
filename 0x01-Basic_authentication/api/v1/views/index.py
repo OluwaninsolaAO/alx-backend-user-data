f64994ab79e3256394476320278d0d6891df6189
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-09 12:26:57 +0100

    Task 01

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,13 +26,13 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized')
+@app_views.route('/unauthorized', strict_slashes=False)
 def unauthorized() -> str:
     """Unauthorized URI"""
     abort(401)
 
 
-@app_views.route('/forbidden')
+@app_views.route('/forbidden', strict_slashes=False)
 def forbidden() -> str:
     """Forbidden URI"""
     abort(403)
