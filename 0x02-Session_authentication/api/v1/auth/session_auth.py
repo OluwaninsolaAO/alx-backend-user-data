56ce11b8cc6176c0291856f02dd30c85e400749c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-10 20:12:54 +0100

    Task 06

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """0x02. Session authentication"""
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,9 @@ class SessionAuth(Auth):
         if any([session_id is None, not isinstance(session_id, str)]):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """Returns the current user from database"""
+        return User.get(self.user_id_for_session_id(
+            self.session_cookie(request)
+        ))
