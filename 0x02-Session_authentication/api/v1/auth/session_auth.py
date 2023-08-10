c2f56c46d3e8760fa24889cb8a30675c28d95e2c
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-10 18:13:01 +0100

    Task 01

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """0x02. Session authentication"""
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session Authentication Definition"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session for a User with the user_id"""
+        if any([user_id is None, not isinstance(user_id, str)]):
+            return None
+        session_id = uuid4().__str__()
+        self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
