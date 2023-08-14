f78da54c5857204c4bd4eae95d3e4a8a40de5e58
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-08-14 09:24:18 +0100

    Remove all test files

--- 0x01-Basic_authentication/6-main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl localhost:5000/api/v1/status
-curl localhost:5000/api/v1/status/
-curl localhost:5000/api/v1/users
-curl localhost:5000/api/v1/users -H 'Authorization: Test'
--- 0x02-Session_authentication/0-main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl localhost:5000/api/v1/status
-curl localhost:5000/api/v1/users
-curl localhost:5000/api/v1/users -H "Authorization: Basic Ym9iQGhidG4uaW86SDBsYmVydG9uU2Nob29sOTgh"
-curl localhost:5000/api/v1/users/me -H "Authorization: Basic Ym9iQGhidG4uaW86SDBsYmVydG9uU2Nob29sOTgh"
